49b0dc3417fc5a4afae9f68ec20f6a85933d1afa
ErbalEssences <LieseErb.com>
2019-04-23 09:54:26 -0500

    fixed sentence structure

--- git-story/short_story.rb
+++ git-story/short_story.rb
@@ -1,7 +1,7 @@
 Sophie is a dog,
-She is a yellow lab,
+She is a yellow lab.
 She likes to be a queen.
-She goes ouside to survey her kingdom,
+She goes ouside to survey her kingdom.
 She is the queen,
 She loves being queen.
 
