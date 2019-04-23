b610af648e49dc8a9088e40fd319511aaed14304
ErbalEssences <LieseErb.com>
2019-04-23 09:52:03 -0500

    made into two sentences

--- git-story/short_story.rb
+++ git-story/short_story.rb
@@ -1,7 +1,7 @@
-Sophie is a dog.
-She is a yellow lab.
+Sophie is a dog,
+She is a yellow lab,
 She likes to be a queen.
-She goes ouside to survey her kingdom.
-She is the queen.
+She goes ouside to survey her kingdom,
+She is the queen,
 She loves being queen.
 
